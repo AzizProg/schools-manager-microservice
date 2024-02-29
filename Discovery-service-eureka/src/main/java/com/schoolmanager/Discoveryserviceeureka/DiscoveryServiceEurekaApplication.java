1588dcd5fd9ddd6946dee655a50524fc068bd03b
AzizProg <traoreabdoulaziz70@gmail.com>
2024-02-29 13:14:23 +0000

    added my services

---
+++ Discovery-service-eureka/src/main/java/com/schoolmanager/Discoveryserviceeureka/DiscoveryServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.Discoveryserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceEurekaApplication.class, args);
+	}
+
+}
---
+++ central-config/src/test/java/com/schoolmanager/centralconfig/CentralConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.centralconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
